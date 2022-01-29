9f79c3190d6d7a23520889e9c2762b065d716ce2
Charles Knipp <charlesknipp98@gmail.com>
2022-01-28 22:14:55 -0500

    misc fixes for SMC^2

--- src/smc_squared.jl
+++ src/smc_squared.jl
@@ -1,6 +1,6 @@
 export SMC2
 
-# this only works with particles that are Vector{Float64}
+# FIX: will sometimes result in bad covariance matrices
 function randomWalk(θ::Particles,kernel::Function,c::Float64=0.5)
     M = length(θ.x)
     x = reduce(hcat,θ.x)
@@ -16,32 +16,36 @@ function randomWalk(θ::Particles,kernel::Function,c::Float64=0.5)
     return Particles([newθ[:,m] for m in 1:M]),pθ
 end
 
-
+# FIX: this is the most inefficient step in the entire process
 function randomWalkMH(
         t::Int64,
         θ::Particles,
         Xt::ParticleSet,
+        y::Vector{Float64},
         N::Int64,
         prior::Function,
+        model,
+        B::Float64,
         c::Float64=0.5,
         chain::Int64=10
     )
 
     # consider whether I should do this per particle or over the whole set
-    k = length(θ.x[1])
     M = length(θ.x)
 
     for _ in 1:chain
-        newθ,pθ = randomWalk(θ,c,prior)
+        newθ,pθ = randomWalk(θ,prior,c)
         newΘ = [StateSpaceModel(model(newθ.x[m]...)) for m in 1:M]
 
         # perform another PF from 1:t and OPTIMIZE THIS
-        newx0 = (newΘ[1].dim_x == 1) ? 0.0 : zeros(Float64,newΘ[1].dim_x)
-        newXt = [Particles(rand(newΘ[m].transition(newx0),N)) for m in 1:M]
+        x0 = (newΘ[1].dim_x == 1) ? 0.0 : zeros(Float64,newΘ[1].dim_x)
+        newXt = [Particles(rand(newΘ[m].transition(x0),N)) for m in 1:M]
+        newXt = ParticleSet(newXt)
 
         for k in 1:t
-            newXt = bootstrapStep(k,newΘ,y,newXt)
-            newθ  = reweight(newθ,newθ.logw+[newXt.p[m].logμ for m in 1:M])
+            newXt = bootstrapStep(k,newΘ,y,newXt,B)
+            newwt = newθ.logw + [newXt.p[m].logμ for m in 1:M]
+            newθ  = Particles(newθ.x,newwt)
         end
 
         # Z(θ) ≡ likelihood, p(θ) ≡ pdf of prior
@@ -58,10 +62,10 @@ function randomWalkMH(
         for m in 1:M
             if u[m] ≤ minimum([α[m],1.0])
                 # resample particle and set the weight w[m] ↤ 1
-                particles[m]   = newθ[m]
+                particles[m]   = newθ.x[m]
                 log_weights[m] = 0.0
 
-                Xt[m] = newXt[m]
+                Xt.p[m] = newXt.p[m]
             end
         end
 
@@ -71,20 +75,28 @@ function randomWalkMH(
     return θ,Xt
 end
 
+# this works surprisingly well
 function bootstrapStep(
         t::Int64,
         Θ::Vector{StateSpaceModel},
         y::Vector{Float64},
-        Xt::ParticleSet
+        Xt::ParticleSet,
+        B::Float64
     )
     M = length(Xt)
 
+    # could parallelize
     for m in 1:M
-        xt = rand.(Θ[m].transition.(Xt.p[m].x))
-        wt = logpdf.(Θ[m].observation.(xt),y[t])
+        # resample the previous state and propogate forward
+        xt = resample(Xt.p[m],B)
+        xt = rand.(Θ[m].transition.(xt.x))
 
+        # weight based on the likelihood of observation
+        wt = logpdf.(Θ[m].observation.(xt),y[t])
         wt += Xt.p[m].logw
-        Xt.p[m] = resample(Particles(xt,wt))
+
+        # create a particle system with appropriate weights
+        Xt.p[m] = Particles(xt,wt)
     end
 
     return Xt
@@ -108,13 +120,15 @@ function SMC2(
 
     # perform iteration t of the bootstrap filter and reweight θ particles
     for t in ProgressBar(1:T)
-        # perform MH steps in case of degeneracy of θ particles
+        # resample with a random walk MH kernel to avoid degeneracy
         if θ.ess < B*M
-            θ,Xt = randomWalkMH(t,θ,Xt,N,prior,0.5,5)
+            θ,Xt = randomWalkMH(t,θ,Xt,y,N,prior,model,0.5,0.8,3)
         end
 
-        Xt = bootstrapStep(t,Θ,y,Xt)
-        θ  = reweight(θ,θ.logw+[Xtm.logμ for Xtm in Xt.p])
+        # propogate forward and reweight
+        Xt = bootstrapStep(t,Θ,y,Xt,B)
+        wt = θ.logw + [Xt.p[m].logμ for m in 1:M]
+        θ  = Particles(θ.x,wt)
     end
 
     return θ,Xt
@@ -133,7 +147,15 @@ function SMC2(
     k = length(θ0)
 
     θ0 = rand(prior(θ0,Matrix{Float64}(I,k,k)),M)
-    θ0 = Particles([θ[:,m] for m in 1:M])
+    θ0 = Particles([θ0[:,m] for m in 1:M])
 
     return SMC2(N,M,y,θ0,prior,B,model)
 end
+
+#=
+IDEA 1: consider changing Base.iterate for particles such that the filter will
+    run at each iteration, and no waste is stored in between iterations
+
+IDEA 2: create an object type for SMC² that stores exogenous characteristics of
+    the algorithm (number of θ particles, number of X particles, model, etc)
+=#
--- test/smc_squared_test.jl
+++ test/smc_squared_test.jl
@@ -3,118 +3,24 @@ include(joinpath(pwd(),"src/sequential_monte_carlo.jl"))
 using .SequentialMonteCarlo
 using Printf,Random,Distributions,LinearAlgebra,StatsBase
 
-#=
-function bootstrapStep(t::Int64,Θ::Vector{StateSpaceModel},y::Vector{Float64},Xt::ParticleSet)
-    M = length(Xt.p)
 
-    for m in 1:M
-        xt = rand.(Θ[m].transition.(Xt.p[m].x))
-        wt = logpdf.(Θ[m].observation.(xt),y[t])
-
-        wt += Xt.p[m].logw
-        Xt.p[m] = resample(Particles(xt,wt))
-    end
-
-    return Xt
-end
-
-function randomWalk(θ::Particles,c::Float64,kernel::Function)
-    M = length(θ.x)
-    x = reduce(hcat,θ.x)
-
-    # calculate the weighted mean
-    μ = vec(mean(x,weights(θ.w),2))
-    
-    # calculate the weighted covariance (please double check)
-    Σ = cov(x,weights(θ.w),2)
-
-    # finish this to generate a particle set
-    pθ = kernel(μ,c*Σ)
-    newθ = rand(pθ,M)
-    return Particles([newθ[:,m] for m in 1:M]),pθ
-end
-
-function randomWalkMH(t::Int64,θ::Particles,Xt::ParticleSet,N::Int64,prior::Function,c::Float64=0.5,chain::Int64=10)
-    # consider whether I should do this per particle or over the whole set
-    k = length(θ)
-    M = length(θ.x)
-
-    for _ in 1:chain
-        newθ,pθ = randomWalk(θ,c,prior)
-        newΘ = [StateSpaceModel(LinearGaussian(newθ.x[m]...)) for m in 1:M]
-
-        # perform another PF from 1:t and OPTIMIZE THIS
-        newx0 = (newΘ[1].dim_x == 1) ? 0.0 : zeros(Float64,newΘ[1].dim_x)
-        newXt = ParticleSet([Particles(rand(newΘ[m].transition(newx0),N)) for m in 1:M])
-
-        for k in 1:t
-            newXt = bootstrapStep(k,newΘ,y,newXt)
-            newθ  = reweight(newθ,newθ.logw+[newXt.p[m].logμ for m in 1:M])
-        end
-
-        # Z(θ) ≡ likelihood, p(θ) ≡ pdf of prior
-        logZt = [Xt.p[m].logμ-newXt.p[m].logμ for m in 1:M]
-        logpt = [logpdf(pθ,θ.x[m])-logpdf(pθ,newθ.x[m]) for m in 1:M]
-
-        # acceptance ratio
-        α = exp.(logZt+logpt)
-        u = rand(M)
-
-        particles   = θ.x
-        log_weights = θ.logw
-
-        for m in 1:M
-            if u[m] ≤ minimum([α[m],1.0])
-                # resample particle and set the weight w[m] ↤ 1
-                particles[m]   = newθ[m]
-                log_weights[m] = 0.0
-
-                Xt[m] = newXt[m]
-            end
-        end
-
-        θ = Particles(particles,log_weights)
-    end
-
-    return θ,Xt
-end
-
-prior(μ,Σ) = TruncatedMvNormal(μ,Σ,[-1.0,-1.0,0.0,0.0],[1.0,1.0,Inf,Inf])
-
-M = 100
-N = 100
-k = 4
-θ0 = [0.7,0.7,1.0,1.0]
-θ = rand(prior(θ0,0.6*Matrix{Float64}(I,k,k)),M)
-θ = Particles([θ[:,m] for m in 1:M])
-
-
-T = length(y)
-
-Θ = [StateSpaceModel(LinearGaussian(θ.x[m]...)) for m in 1:M]
-x0 = (Θ[1].dim_x == 1) ? 0.0 : zeros(Float64,Θ[1].dim_x)
-Xt = ParticleSet([Particles(rand(Θ[m].transition(x0),N)) for m in 1:M])
-
-for t in ProgressBar(1:T)
-    global Xt = bootstrapStep(t,Θ,y,Xt)
-    global θ  = reweight(θ,θ.logw+[Xtm.logμ for Xtm in Xt.p])
-    
-    # perform MH steps in case of degeneracy of θ particles
-    if θ.ess < 0.5*M
-        global θ,Xt = randomWalkMH(t,θ,Xt,N,prior,0.5,4)
-    end
-end
-=#
-
-
-# simulate shit
+# simulate data
 test_params = LinearGaussian(0.8,0.5,1.0,1.0)
 test_model  = StateSpaceModel(test_params)
-x,y = simulate(test_model,200)
+x,y = simulate(test_model,50)
 
+# construct a prior distribution
 prior(μ,Σ) = TruncatedMvNormal(μ,Σ,[-1.0,-1.0,0.0,0.0],[1.0,1.0,Inf,Inf])
 θ0 = [0.7,0.7,1.0,1.0]
 
-θ,Xt = SMC2(100,100,y,θ0,prior,0.5,LinearGaussian)
+θ,Xt = SMC2(20,100,y,θ0,prior,0.5,LinearGaussian)
 
 mean(reduce(hcat,θ.x),weights(θ.w),2)
+
+# [println(Xtm.logμ) for Xtm in Xt.p]
+
+#=
+    The output looks as it should, but the weights that are generated are not
+    what they seem. The algorithm spits out an evenly weighted particle cloud
+    which should not be the case
+=#
