b55650550b732639590710ce2116d3c9eda17e50
Charles Knipp <charlesknipp98@gmail.com>
2022-02-16 14:31:54 -0500

    added bisection method

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,16 +1,16 @@
-export randTruncatedMvNormal,logpdfTruncatedMvNormal,ProgressBar,secant
+export randTruncatedMvNormal,logpdfTruncatedMvNormal,ProgressBar,bisection
 
 using Printf
 
 function secant(
-        f::Function,x0::Number,x1::Number;
-        tol::AbstractFloat=1e-5,maxiter::Int64=50
+        f::Function,x0::Float64,x1::Float64;
+        tol::AbstractFloat=1.e-12,maxiter::Int64=50
     )
 
     for _ in 1:maxiter
         y1 = f(x1)
         y0 = f(x0)
-        x  = (x1-y1)*(x1-x0)/(y1-y0)
+        x  = x1-y1*(x1-x0)/(y1-y0)
 
         if abs(x-x1) < tol
             return x
@@ -22,6 +22,39 @@ function secant(
 
     # throw an error upon exceeding max_iters
     error("Max iteration exceeded")
+end
+
+function bisection(
+        f::Function,lower_bound::Number,upper_bound::Number;
+        tol::AbstractFloat=1e-12,maxiter::Integer=50
+    )
+
+    f_lower_bound = f(lower_bound)
+    # f_lower_bound*f(upper_bound) <= 0 || error("No real root")
+
+    i = 0
+    midpoint = upper_bound
+
+    while upper_bound-lower_bound > tol
+        i += 1
+        i != maxiter || error("Max iteration exceeded")
+
+        midpoint   = (lower_bound+upper_bound)/2
+        f_midpoint = f(midpoint)
+
+        if f_midpoint == 0
+            break
+        elseif f_lower_bound*f_midpoint > 0
+            # Root is in the right half of [a,b]
+            lower_bound   = midpoint
+            f_lower_bound = f_midpoint
+        else
+            # Root is in the left half of [a,b]
+            upper_bound = midpoint
+        end
+    end
+
+    return lower_bound
 end
 
 
