327662cd1bf17cca81ac1ef104cf0911e9eeac4b
Charles Knipp <charlesknipp98@gmail.com>
2022-02-17 16:21:25 -0500

    various bug fixes

--- src/density_tempered_smc.jl
+++ src/density_tempered_smc.jl
@@ -3,17 +3,11 @@ export densityTemperedSMC
 function gridSearch(ξ::Float64,θ::Particles,B::Float64)
     # solve for such that θ.ess == B*M
     M = length(θ)
+
     f(Δ::Float64) = ESS(Δ*θ.logw) - B*M
+    δ = bisection(f,1.e-12,1-ξ)
 
-    if f(1.0-ξ) > 0
-        δ = 1.0 - ξ
-        newξ = 1.0
-    else
-        δ = secant(f,1.e-12,1-ξ)
-        newξ = ξ + δ
-    end
-
-    return newξ
+    return ξ+δ
 end
 
 # FIX: will sometimes result in bad covariance matrices
@@ -33,30 +27,37 @@ end
 
 # this is totally untested, but the logic is 100% there
 function randomWalkMH(
-        θ::Particles,model,N::Int64,y::Vector{Float64},pθ::Sampleable;
+        θ::Particles,model,N::Int64,y::Vector{Float64},pθ::Sampleable,ξ::Float64;
         B::Float64=0.5,c::Float64=0.5,len_chain::Int64=5
     )
 
     M = length(θ)
+    acc = zeros(Int64,M)
 
     for _ in 1:len_chain
         newθ = randomWalk(θ,c)
 
         # can be parallelized
         for m in 1:M
-            newΘm = StateSpaceModel(model(newθ[m]...))
+            newΘm = StateSpaceModel(model(newθ.x[m]...))
             newXm = bootstrapFilter(N,y,newΘm,B)
             newθ.logw[m] = sum([Xmt.logμ for Xmt in newXm.p])
 
-            αm = ξ*(newθ.logw[m]-θ.logw[m])
-            αm += (logpdf(pθ,θ)-logpdf(pθ,newθ))
+            αm = ξ*newθ.logw[m]-θ.logw[m]
+            αm += (logpdf(pθ,θ.x[m])-logpdf(pθ,newθ.x[m]))
 
             if rand() ≤ minimum([αm,1.0])
-                θ.x = newθ
-                θ.logw = 0.0
+                θ.x[m] = newθ[m]
+
+                # not sure how to set this weight
+                θ.logw[m] = 0.0
+                acc[m] = 1
             end
         end
     end
+
+    acc_rate = mean(acc) / M
+    println(acc_rate)
 
     # re-normalize everything
     return Particles(θ.x,θ.logw)
@@ -71,7 +72,10 @@ function densityTemperedSMC(
         B::Float64 = 0.5,
         model = LinearGaussian
     )
-    
+
+    k = length(θ0)
+    ξ = 1.e-12
+
     # initialize particles
     pθ = prior(θ0,Matrix{Float64}(I,k,k))
     θ = rand(pθ,M)
@@ -80,19 +84,22 @@ function densityTemperedSMC(
     # parallelize this
     for m in 1:M
         Θm = StateSpaceModel(model(θ.x[m]...))
-        Xm = bootstrapFilter(N,y,Θm,0.5)
+        Xm = bootstrapFilter(N,y,Θm)
         θ.logw[m] = sum([Xmt.logμ for Xmt in Xm.p])
     end
 
-    while ξ ≤ 1.0
+    while ξ < 1.0
         # find exponent ξ, reweight, and resample
         newξ = gridSearch(ξ,θ,B)
         θ = reweight(θ,[(newξ-ξ)*θ.logw[m] for m in 1:M])
-        θ = resample(θ,B)   # evaluates condition unnecessarily
 
         if θ.ess < B*M
-            θ = randomWalkMH(θ,model,N,y,pθ)
+            θ = resample(θ)
+            θ = randomWalkMH(θ,model,N,y,pθ,newξ)
         end
+
+        println(newξ)
+        ξ = newξ
     end
 
     return θ
