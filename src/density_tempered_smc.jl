8169ed5ef931585d7b30363bc3e6193ef8578e89
Charles Knipp <charlesknipp98@gmail.com>
2022-02-22 14:42:33 -0500

    apply new bootstrap filter method

--- src/density_tempered_smc.jl
+++ src/density_tempered_smc.jl
@@ -21,6 +21,32 @@ mutable struct DensityTemperedSMC
     ess::Float64
 end
 
+# rewritten bootstrap method, slightly faster (emphasis on slightly)
+function bootstrapLikelihood(N::Int64,y::Vector{Float64},prior::StateSpaceModel)
+    T = length(y)
+
+    # initialize algorithm
+    logZ = zeros(Float64,T)
+
+    # in the case where x0 is located at the origin
+    x0 = (prior.dim_x == 1) ? 0.0 : zeros(Float64,prior.dim_x)
+    p0 = Particles(rand(prior.transition(x0),N))
+
+    logZ[1] = p0.logμ
+    ps = resample(p0)
+
+    for t in 2:T
+        xt = rand.(prior.transition.(ps.x))
+        wt = logpdf.(prior.observation.(xt),y[t])
+        pt = Particles(xt,wt)
+
+        logZ[t] = pt.logμ
+        ps = resample(pt)
+    end
+
+    return logZ
+end
+
 function reweight!(smc::DensityTemperedSMC)
     smc.iteration += 1
 
@@ -60,7 +86,7 @@ function reweight!(smc::DensityTemperedSMC)
     smc.ξ = newξ
 
     # display the tempering sequence in the console
-    @printf("ξ[%0d] = %0.6f\u1b[K\n",smc.iteration,smc.ξ)
+    @printf("ξ[%0d] = %0.6f\n",smc.iteration,smc.ξ)
 
     # take the log of the normalized weights to reduce numerical problems
     w = exp.(logw)
@@ -85,15 +111,13 @@ function rejuvinate!(smc::DensityTemperedSMC,c::Float64,len_chain::Int64=5)
 
     # kinda sussy
     newθ = rand(MvNormal(μ,c*Σ),smc.M)
-    newθ[3:4,:] = abs.(newθ[3:4,:])
 
     for _ in 1:len_chain
         u = rand(smc.M)
         for m in 1:smc.M
             # TODO: implement new SSM behavior
             Θm = StateSpaceModel(smc.model(newθ[:,m]...))
-            newXm = bootstrapFilter(smc.N,smc.y,Θm)
-            logZm = sum([Xmt.logμ for Xmt in newXm.p])
+            logZm = sum(bootstrapLikelihood(smc.N,smc.y,Θm))
 
             # TODO: store logpdf(prior,θ) to avoid redundancies
             αm = smc.ξ*(logZm-smc.logZ[m])
@@ -108,7 +132,7 @@ function rejuvinate!(smc::DensityTemperedSMC,c::Float64,len_chain::Int64=5)
     end
 
     # reset the cursor to print ξ to stdout
-    print("\r")
+    print("\r\u1b[K")
 end
 
 # TODO: add [rng=GLOBAL_RNG] to aid in testing
@@ -132,8 +156,7 @@ function densityTemperedSMC(
     logZ = zeros(Float64,M)
     for m in 1:M
         Θm = StateSpaceModel(model(θ[m]...))
-        Xm = bootstrapFilter(N,y,Θm)
-        logZ[m] = sum([Xmt.logμ for Xmt in Xm.p])
+        logZ[m] = sum(bootstrapLikelihood(N,y,Θm))
     end
 
     # initialize weights at 1/M
