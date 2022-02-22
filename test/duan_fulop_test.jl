6eebdf5dc1d857fe6926177d509ed5fd85a9d014
Charles Knipp <charlesknipp98@gmail.com>
2022-02-22 14:43:10 -0500

    new test case

--- test/duan_fulop_test.jl
+++ test/duan_fulop_test.jl
@@ -5,14 +5,15 @@ using Printf,Random,Distributions,LinearAlgebra,StatsBase
 
 test_params = LinearGaussian(0.8,0.5,1.0,1.0)
 test_model  = StateSpaceModel(test_params)
-x,y = simulate(test_model,50)
+x,y = simulate(test_model,1000)
 
-prior(μ,Σ) = TruncatedMvNormal(μ,Σ,[-1.0,-1.0,0.0,0.0],[1.0,1.0,Inf,Inf])
-linGauss(A,B,Q,R) = LinearGaussian(A,B,Q,R)
+prior(μ,Σ) = TruncatedMvNormal(μ,Σ,[-1.0,-1.0,-Inf,-Inf],[1.0,1.0,Inf,Inf])
+linGauss(A,B,Q,R) = LinearGaussian(A,B,exp(Q),exp(R))
 
-θ,w = densityTemperedSMC(100,100,y,prior,[0.3,0.4,1.0,1.0],linGauss)
+θ,w = densityTemperedSMC(100,100,y,prior,[0.3,0.4,0.0,0.0],linGauss)
 
 meanθ = mean(reduce(hcat,θ),weights(w),2)
+meanθ[3:4] = exp.(meanθ[3:4])
 
 # print output to console
 println()
