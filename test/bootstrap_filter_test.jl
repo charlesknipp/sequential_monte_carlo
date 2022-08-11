346e182c14a7f402a0d280bc7a4ec7c7e5ddb024
Charles Knipp <32943413+classicharlie@users.noreply.github.com>
2022-08-11 12:35:07 -0400

    update test cases

--- test/bootstrap_filter_test.jl
+++ test/bootstrap_filter_test.jl
@@ -1,71 +1,47 @@
 include(joinpath(pwd(),"src/sequential_monte_carlo.jl"))
 
-using .SequentialMonteCarlo
-using Printf,Random,Distributions
 
-test_params = LinearGaussian(1.0,1.0,1.0,1.0)
-test_model  = StateSpaceModel(test_params)
+using Distributed
+@everywhere using .SequentialMonteCarlo
+@everywhere using Printf,Random,Distributions
 
-# initializing needs some work
-Random.seed!(123)
-x,y = simulate(test_model,100)
+lg = StateSpaceModel(LinearGaussian(1.0,1.0,1.0,1.0,0.0,1.0),(1,1))
+_,y = simulate(lg,10)
 
-Random.seed!(123)
-xs_bf,logZ_bf = bootstrapFilter(1000,y,test_model,Inf)
-xs_kf,logZ_kf = kalmanFilter(y,test_params)
+pf = ParticleFilter(100,lg,1.0)
+logZ = log_likelihood(pf,y)
 
-for t in 1:100
-    println(@sprintf("logZ_bf: %.5f\tlogZ_kf: %.5f",logZ_bf[t],logZ_kf[t]))
-end
+## PARALLELIZING ##############################################################
 
+rng = Random.GLOBAL_RNG
+M = 10
+N = 100
+
+prior = product_distribution([
+    TruncatedNormal(0,1,-1,1),
+    LogNormal(),
+    LogNormal()
+])
+
+mod_func(θ) = StateSpaceModel(
+    LinearGaussian(θ[1],1.0,θ[2],θ[3],0.0,1.0),
+    (1,1)
+)
+
+θ  = Particles{Vector{Float64}}([rand(rng,prior) for _ in 1:M])
+pf = [ParticleFilter(rng,N,mod_func(θ.x[i])) for i in 1:M]
+
+pool = CachingPool(workers())
+
+## BENCHMARKING ###############################################################
 
 using BenchmarkTools
 
-# rewritten bootstrap method, slightly faster (emphasis on slightly)
-function bf(N::Int64,y::Vector{Float64},prior::StateSpaceModel)
-    T = length(y)
+# benchmarks suggest that this is faster than the original implementation taken
+# from LowLevelParticleFilters.jl
 
-    # initialize algorithm
-    logZ = zeros(Float64,T)
+# single threaded process
+@benchmark map(x -> log_likelihood(x,$y),$pf)
 
-    # in the case where x0 is located at the origin
-    x0 = (prior.dim_x == 1) ? 0.0 : zeros(Float64,prior.dim_x)
-    p0 = Particles(rand(prior.transition(x0),N))
-
-    logZ[1] = p0.logμ
-    ps = resample(p0)
-
-    for t in 2:T
-        xt = rand.(prior.transition.(ps.x))
-        wt = logpdf.(prior.observation.(xt),y[t])
-        pt = Particles(xt,wt)
-
-        logZ[t] = pt.logμ
-        ps = resample(pt)
-    end
-
-    return logZ
-end
-
-Random.seed!(123)
-logZ_bf1 = bf(1000,y,test_model)
-
-println()
-for t in 1:100
-    println(@sprintf("logZ_bf: %.5f\tlogZ_bf1: %.5f",logZ_bf[t],logZ_bf1[t]))
-end
-
-function timebf1()
-    Random.seed!(123)
-    _,logZ = bootstrapFilter(1000,y,test_model,Inf)
-    return logZ
-end
-
-function timebf2()
-    Random.seed!(123)
-    logZ = bf(1000,y,test_model)
-    return logZ
-end
-
-@benchmark timebf1()
-@benchmark timebf2()
+# multithreaded process
+@benchmark pmap(x -> log_likelihood(x,$y),pool,$pf)
