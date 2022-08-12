f2bd1eb6066d6fcc85ad6839b9173688ea437e6f
Charles Knipp <charlesknipp98@gmail.com>
2022-08-12 19:31:41 -0400

    branch clean-up

--- src/sequential_monte_carlo.jl
+++ src/sequential_monte_carlo.jl
@@ -1,16 +1,14 @@
 module SequentialMonteCarlo
 
-using Distributions,LinearAlgebra,StatsBase,Statistics,Printf
+using Distributions,LinearAlgebra,StatsBase,Statistics,Printf,Random
 
 import StatsBase:  cov,mean,params,mode,median
 import Statistics: cov,mean,quantile,median
 
 include("state_space_model.jl")
 include("particles.jl")
-include("utilities.jl")
 include("particle_filter.jl")
 include("smc_squared.jl")
-include("probability_densities.jl")
 include("density_tempered_smc.jl")
 
 end
