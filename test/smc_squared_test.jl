930c410dd697a4fb34bba06635c38b8818f40921
Charles Knipp <charlesknipp98@gmail.com>
2022-08-12 19:35:47 -0400

    update test case for smc²

--- test/smc_squared_test.jl
+++ test/smc_squared_test.jl
@@ -11,12 +11,12 @@ prior = product_distribution([
 ])
 
 mod_func(θ) = StateSpaceModel(
-    LinearGaussian(θ[1],1.0,exp(θ[2]),exp(θ[3]),0.0,1.0),
+    LinearGaussian(θ[1],1.0,θ[2],θ[3],0.0,1.0),
     (1,1)
 )
 
 
-alg = SMC²(100,100,prior,mod_func,4,0.5,Random.GLOBAL_RNG)
+alg = SMC²(512,100,prior,mod_func,3,0.8,Random.GLOBAL_RNG)
 
 test_mod = mod_func([0.5,1.0,1.0])
 x,y = simulate(test_mod,100)
@@ -24,3 +24,12 @@ x,y = simulate(test_mod,100)
 for i in 1:100
     update!(alg,y)
 end
+
+# double check that particle filters indexed properly
+for i in 1:100 println(alg.filters[i].state.t[]) end
+
+# find the weighted mean of the particles
+sum(
+    alg.parameters.x .* alg.parameters.w,
+    dims = 1
+)
