52610e398f4b9525ec9eb8f37ebea917cc67a829
Charles Knipp <charlesknipp98@gmail.com>
2022-08-22 14:28:34 -0400

    added stochastic volatility animation

--- test/sv_demonstration.jl
+++ test/sv_demonstration.jl
@@ -2,7 +2,9 @@ include(joinpath(pwd(),"src/sequential_monte_carlo.jl"))
 
 using .SequentialMonteCarlo
 using Printf,Random,Distributions,LinearAlgebra,Plots
+using Plots.PlotMeasures
 
+#=
 function plot_histograms(smc::SMC,labels::AbstractVector)
     θ   = reduce(hcat,smc.θ)
     plt = plot(
@@ -14,6 +16,34 @@ function plot_histograms(smc::SMC,labels::AbstractVector)
 
     return plt
 end
+=#
+
+function plot_histograms(smc::SMC,labels::AbstractVector)
+    θ = reduce(hcat,smc.θ)
+    plots = map(
+        i -> histogram(
+            θ[i,:],
+            weights = smc.ω,
+            normalize = :probability,
+            xlabel = labels[i],
+            grid = false,
+            tickdir = :out,
+            bins = 25
+        ),
+        1:length(labels)
+    )
+
+    return plot(
+        plots...,
+        legend = false,
+        layout = (1,length(labels)),
+        size = (1200,500),
+        framestyle = :box,
+        top_margin = 15mm,
+        margin = 10mm,
+        bottom_margin = 10mm
+    )
+end
 
 sv_prior = product_distribution([
     Normal(0.0,2.0),
@@ -22,11 +52,11 @@ sv_prior = product_distribution([
 ])
 
 sv_mod(θ) = StateSpaceModel(
-    StochasticVolatility(θ...,nothing),
+    StochasticVolatility(θ...),
     (1,1)
 )
 
-T = 1000
+T = 5000
 
 θ = [-1.0,0.9,0.2]
 x,y = simulate(sv_mod(θ),T)
@@ -43,13 +73,16 @@ dt_plot_2 = plot_histograms(sv_dt_smc,["μ","ρ","σ"])
 
 
 ## smc²
-sv_smc²     = SMC(Random.GLOBAL_RNG,512,1024,sv_mod,sv_prior,3,0.5)
-smc²_plot_1 = plot_histograms(sv_smc²,["μ","ρ","σ"])
-
+sv_smc² = SMC(Random.GLOBAL_RNG,512,512,sv_mod,sv_prior,3,0.5)
 smc²(sv_smc²,y)
-for t in 2:T
-    smc²!(sv_smc²,y,t)
-end
 
+
+plt = @animate for t in 2:T
+    smc²!(sv_smc²,y,t)
+    plot_histograms(sv_smc²,["μ","ρ","σ"])
+    plot!(plot_title=string("\n",t))
+end every 10
+
+gif(plt,"stochastic_volatility_animation.gif",fps=15)
 expected_parameters(sv_smc²)
 smc²_plot_2 = plot_histograms(sv_smc²,["μ","ρ","σ"])
