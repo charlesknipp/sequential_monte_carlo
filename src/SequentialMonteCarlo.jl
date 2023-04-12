020ed19376ec8d88b80954783e9a97ccb068a63d
Charles Knipp <32943413+classicharlie@users.noreply.github.com>
2023-04-12 13:59:51 -0400

    update SequentialMonteCarlo.jl

--- src/SequentialMonteCarlo.jl
+++ src/SequentialMonteCarlo.jl
@@ -1,10 +1,28 @@
 module SequentialMonteCarlo
 
-using Distributions,LinearAlgebra,Statistics,Printf,Random,StatsBase,StaticArrays
+using Distributions,LinearAlgebra,Statistics,Printf,Random,StatsBase
+import Statistics: quantile
 
-include("state_space_model.jl")
-include("particles.jl")
+# state space object definition
+include("state_space_models.jl")
+
+# for generalized state space models
+include("particle_filter.jl")
 include("smc_samplers.jl")
+
+# for linear models
+include("kalman_filter.jl")
+include("ibis.jl")
+
+# in and out of sample forecasting
+include("forecast.jl")
+
+# custom plotting functions (WIP)
+using PGFPlotsX,LaTeXStrings,DataFrames,ColorBrewer
+include("plotting_utils.jl")
+
+# fast multivariate distributions
+using PDMats,StaticArrays
 include("utilities.jl")
 
 end
