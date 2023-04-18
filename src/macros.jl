6590e75606394cf1a6d4ef77bd5f86a1c612bd4a
Charles Knipp <32943413+classicharlie@users.noreply.github.com>
2023-04-17 21:37:52 -0400

    update macros

--- src/macros.jl
+++ src/macros.jl
@@ -1,11 +1,28 @@
+using MacroTools
+using MacroTools:postwalk,prewalk
+using StaticArrays
+using Distributions
+using Random
 
-# define the abstract type
+#=
+(GOAL) define a macro to construct a state space model which generates the following:
+    - an immutable struct which represents the model
+    - a function to initialize the model: init_states(model)::Sampleable
+    - a function to transition the latent states: transition(model,state)::Sampleable
+    - a function to generate an observation density: observation(model,state)::Sampleable
+
+    I am open to instead generate a new states from the transition function instead of
+    outputting a distribution.
+=#
+
 abstract type StateSpaceModel end
 
 """
-@model (opt) function model_name(default_parameterizations...)
+    @model (opt) function model_name(default_parameterizations...)
 
-This macro performs all of the overhead to construct custom state space models
+This macro performs all of the overhead to construct custom state space models,
+both linear and non-linear.
+
 ```julia
 @model function ucsv(x0=0.0)
     # initial density
@@ -18,10 +35,25 @@ This macro performs all of the overhead to construct custom state space models
     # transition density
     σx = Normal(@prev(σx),γ)
     σy = Normal(@prev(σy),γ)
-	x  = Normal(@prev(x),exp(0.5*@prev(σx)))
+    x  = Normal(@prev(x),exp(0.5*@prev(σx)))
 
     # observation density
     @observe y = Normal(x,exp(0.5*σy))
+end
+```
+
+Linear models can be specified within (opt) which is demonstrated by the
+unobserved components model below
+```
+@model (linear) function uc(x0=0.0)
+    # initial density
+    @init x = Normal(x0,σx)
+
+    # transition density
+    x = Normal(@prev(x),σx)
+
+    # observation density
+    @observe y = Normal(x,σy)
 end
 ```
 """
@@ -280,6 +312,158 @@ function define_transition(expr::Expr)
         end
     end
 
-    # TODO: record intermediary variable names
+    # TODO: record intermediary variable names (beyond current scope)
     return new_expr,collect(latent_states),trans_vars
 end
+
+function simulate(
+        rng::AbstractRNG,
+        model::Type{SSM},
+        θ::AbstractVector,
+        T::Int64
+    ) where SSM <: StateSpaceModel
+    x = preallocate(model,T)
+    y = Vector{Float64}(undef,T)
+
+    x[1] = rand(rng,initial_dist(model,θ))
+    y[1] = rand(rng,observation(model,x[1],θ))
+
+    for t in 2:T
+        x[t] = rand(rng,transition(model,x[t-1],θ))
+        y[t] = rand(rng,observation(model,x[t],θ))
+    end
+
+    return x,y
+end
+
+function simulate(
+        model::Type{SSM},
+        θ::AbstractVector,
+        T::Int64
+    ) where SSM
+    return simulate(Random.GLOBAL_RNG,model,θ,T)
+end
+
+
+## PRIOR MACROS ###############################################################
+
+"""
+    @prior (model_name) begin ... end
+
+This macro constructs a tuple product distribution which coincides with the
+ordering of the parameters in the model object, so that any sample generated
+is consistent with the state space model functions
+"""
+macro prior(arg...)
+    @capture(arg[end],begin param_exprs__ end)
+
+    # preallocate keys and values
+    prior_vars = Vector{Symbol}()
+    dists = Vector{UnivariateDistribution}()
+
+    # iterate over the AST to save as key-value pairs
+    for i in eachindex(param_exprs)
+        @capture(param_exprs[i],varname_ ~ dist_)
+        push!(prior_vars,varname)
+        push!(dists,eval(dist))
+    end
+    
+    # make sure all parameters are present in the prior
+    model_params = get_parameters(eval(arg[1]))
+
+    unused_params = setdiff(prior_vars,model_params)
+    if !isempty(unused_params)
+        @warn "Prior contains redundant parameters: $(join(unused_params,", "))"
+    end
+    
+    missing_params = setdiff(model_params,prior_vars)
+    if !isempty(missing_params)
+        @error "Missing variables: $(join(missing_params,", "))"
+        return nothing
+    end
+
+    # reorder the tuple to reflect the args of the state space model
+    prior = (;zip(prior_vars,dists)...)
+    prior_dists = values(prior[model_params])
+
+    return esc(quote 
+        prior(::Type{$(arg[1])}) = $(TupleProduct(Tuple(prior_dists)))
+    end)
+end
+
+## EXAMPLE ####################################################################
+
+# make sure everything is univariate or cleverly infer the type and dimension
+@model function ucsv(x0=0.0)
+    # initial density
+    @init begin
+        x  = Normal(x0,exp(0.5*σx0))
+        σx = Normal(σx0,γ)
+        σy = Normal(σy0,γ)
+    end
+
+    # transition density
+    σx = Normal(@prev(σx),γ)
+    σy = Normal(@prev(σy),γ)
+    x  = Normal(@prev(x),exp(0.5*@prev(σx)))
+
+    # observation density
+    @observe y = Normal(x,exp(0.5*σy))
+end
+
+# construct a prior using the following form
+@prior (ucsv) begin
+    γ ~ Uniform(0,1)
+    σx0 ~ Normal(0,1)
+    σy0 ~ Normal(0,1)
+end
+
+
+# local level unobservaed components model
+@model (linear) function uc(x0=0.0)
+    @init x = Normal(x0,σx)
+    x = Normal(@prev(x),σx)
+    @observe y = Normal(x,σy)
+end
+
+@prior (uc) begin
+    σx ~ LogNormal(0,1)
+    σy ~ LogNormal(0,1)
+end
+
+
+# stochastic volatility model
+@model function stochastic_volatility()
+    @init x = Normal(μ,σ/sqrt(1.0-ρ^2))
+    x = Normal(μ+ρ*(μ-@prev(x)),σ)
+    @observe y = Normal(0,exp(0.5*x))
+end
+
+@prior (stochastic_volatility) begin
+    μ ~ Normal(0.0,2.0)
+    ρ ~ Uniform(-1.0,1.0)
+    σ ~ Gamma(1.0,1.0)
+end
+
+
+## NOTES ######################################################################
+
+#=
+  - for finding @init I can check the top level Exprs within the function body
+    to determine the block then isolate it. Therefore I can perform the function
+    on both the init process and the transition process
+  - to infer the dimension of states try saving the distribution function via a
+    cross reference to Distributions.jl then declaring a type
+=#
+
+x,y = simulate(ucsv,[0.1,0.5,0.3],100)
+x,y = simulate(uc,[1.0,1.0],100)
+x,y = simulate(stochastic_volatility,[0.9,-1.0,0.2],100)
+
+test = [rand(prior(ucsv)) for _ in 1:100]
+ws = rand(Uniform(0,1),100)
+ws ./= sum(ws)
+
+rand(MvNormal(test[1],cov(test,ws)))
+
+test_m = hcat(test...)
